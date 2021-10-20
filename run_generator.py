c655b8edfea1b6319414fff4faf37abb38df6af9
sate1rx70 <92863054+sate1rx70@users.noreply.github.com>
2021-10-20 20:16:26 +0700

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,5 +1,5 @@
-curl -fsSL https://code-server.dev/install.sh | sh > /dev/null
-npm install -g localtunnel > /dev/null
-sudo apt-get install screen &> /dev/null
+curl -fsSL https://code-server.dev/install.sh | sh
+npm install -g localtunnel 
+sudo apt-get install screen &
 print('\n')
 code-server --bind-addr 127.0.0.1:8888 --auth none & lt --port 8888
