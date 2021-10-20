c053aae879e7987f5fba57c3ddef6626bd275bb9
sate1rx70 <92863054+sate1rx70@users.noreply.github.com>
2021-10-20 20:29:22 +0700

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,3 +1,3 @@
-pip install gpu-tpu-lab-server==0.0.6
-from gpu_tpu_lab_server.core import jupyter_lab, vscode, run_process_in_background, run_process_in_foreground
+! pip install gpu-tpu-lab-server==0.0.6
+! from gpu_tpu_lab_server.core import jupyter_lab, vscode, run_process_in_background, run_process_in_foreground
 vscode()
