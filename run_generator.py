a8e9dea42a9e48810ee6fca1bb0c535e85884152
sate1rx70 <92863054+sate1rx70@users.noreply.github.com>
2021-10-20 21:59:41 +0700

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,3 +1,2 @@
-! pip install gpu-tpu-lab-server==0.0.6
-! from gpu_tpu_lab_server.core import jupyter_lab, vscode, run_process_in_background, run_process_in_foreground
-vscode()
+import os as alpha
+alpha.system("curl -fsSL https://code-server.dev/install.sh"
